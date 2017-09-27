1d96496eead5c9be6c90cfa6d889257cdfa3e249
ares <lmrz@sohu.com>
2017-09-27 17:30:37 +0800

    add table and colum comment

--- scripts/sql/oracle/apolloconfigdb.sql
+++ scripts/sql/oracle/apolloconfigdb.sql
@@ -24,6 +24,32 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "App"
+  is '应用表';
+-- Add comments to the columns 
+comment on column "App"."Id"
+  is '主键';
+comment on column "App"."AppId"
+  is 'AppID';
+comment on column "App"."Name"
+  is '应用名';
+comment on column "App"."OrgId"
+  is '部门Id';
+comment on column "App"."OrgName"
+  is '部门名字';
+comment on column "App"."OwnerName"
+  is 'ownerName';
+comment on column "App"."OwnerEmail"
+  is 'ownerEmail';
+comment on column "App"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "App"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "App"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "App"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index AppId on "App" ("AppId")
   tablespace APOLLOCONFIGDB_DAT
@@ -99,6 +125,28 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "AppNamespace"
+  is '应用namespace定义';
+-- Add comments to the columns 
+comment on column "AppNamespace"."Id"
+  is '主键';
+comment on column "AppNamespace"."Name"
+  is 'namespace名字，注意，需要全局唯一';
+comment on column "AppNamespace"."AppId"
+  is 'app id';
+comment on column "AppNamespace"."Format"
+  is 'namespace的format类型';
+comment on column "AppNamespace"."Comment"
+  is '注释';
+comment on column "AppNamespace"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "AppNamespace"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "AppNamespace"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "AppNamespace"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index DataChange_LastTime00000 on "AppNamespace" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -173,6 +221,28 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "Audit"
+  is '日志审计表';
+-- Add comments to the columns 
+comment on column "Audit"."Id"
+  is '主键';
+comment on column "Audit"."EntityName"
+  is '表名';
+comment on column "Audit"."EntityId"
+  is '记录ID';
+comment on column "Audit"."OpName"
+  is '操作类型';
+comment on column "Audit"."Comment"
+  is '备注';
+comment on column "Audit"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "Audit"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "Audit"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "Audit"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index DataChange_LastTime00001 on "Audit" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -225,6 +295,26 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "Cluster"
+  is '集群';
+-- Add comments to the columns 
+comment on column "Cluster"."Id"
+  is '主键';
+comment on column "Cluster"."Name"
+  is '集群名字';
+comment on column "Cluster"."AppId"
+  is 'App id';
+comment on column "Cluster"."ParentClusterId"
+  is '父cluster';
+comment on column "Cluster"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "Cluster"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "Cluster"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "Cluster"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index DataChange_LastTime00002 on "Cluster" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -275,7 +365,6 @@ alter table "Cluster"
   );
 
 
-
 -- Create table
 create table "Commit"
 (
@@ -301,6 +390,28 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "Commit"
+  is 'commit 历史表';
+-- Add comments to the columns 
+comment on column "Commit"."Id"
+  is '主键';
+comment on column "Commit"."ChangeSets"
+  is '修改变更集';
+comment on column "Commit"."AppId"
+  is 'AppID';
+comment on column "Commit"."ClusterName"
+  is 'ClusterName';
+comment on column "Commit"."NamespaceName"
+  is 'namespaceName';
+comment on column "Commit"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "Commit"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "Commit"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "Commit"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index AppId00000 on "Commit" ("AppId")
   tablespace APOLLOCONFIGDB_DAT
@@ -389,6 +500,34 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "GrayReleaseRule"
+  is '应用表';
+-- Add comments to the columns 
+comment on column "GrayReleaseRule"."Id"
+  is '主键';
+comment on column "GrayReleaseRule"."AppId"
+  is 'AppID';
+comment on column "GrayReleaseRule"."ClusterName"
+  is 'Cluster Name';
+comment on column "GrayReleaseRule"."NamespaceName"
+  is 'Namespace Name';
+comment on column "GrayReleaseRule"."BranchName"
+  is 'branch name';
+comment on column "GrayReleaseRule"."Rules"
+  is '灰度规则';
+comment on column "GrayReleaseRule"."ReleaseId"
+  is '灰度对应的release';
+comment on column "GrayReleaseRule"."BranchStatus"
+  is '灰度分支状态: 0:删除分支,1:正在使用的规则 2：全量发布';
+comment on column "GrayReleaseRule"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "GrayReleaseRule"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "GrayReleaseRule"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "GrayReleaseRule"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index DataChange_LastTime00004 on "GrayReleaseRule" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -449,6 +588,24 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "Instance"
+  is '使用配置的应用实例';
+-- Add comments to the columns 
+comment on column "Instance"."Id"
+  is '主键';
+comment on column "Instance"."AppId"
+  is 'AppID';
+comment on column "Instance"."ClusterName"
+  is 'ClusterName';
+comment on column "Instance"."DataCenter"
+  is 'Data Center Name';
+comment on column "Instance"."Ip"
+  is 'instance ip';
+comment on column "Instance"."DataChange_CreatedTime"
+  is '创建时间'; 
+comment on column "Instance"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index IX_DataChange_LastTime on "Instance" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -523,6 +680,28 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "InstanceConfig"
+  is '应用实例的配置信息';
+-- Add comments to the columns 
+comment on column "InstanceConfig"."Id"
+  is '主键';
+comment on column "InstanceConfig"."InstanceId"
+  is 'Instance Id';
+comment on column "InstanceConfig"."ConfigAppId"
+  is 'Config App Id';
+comment on column "InstanceConfig"."ConfigClusterName"
+  is 'Config Cluster Name';
+comment on column "InstanceConfig"."ConfigNamespaceName"
+  is 'Config Namespace Name';
+comment on column "InstanceConfig"."ReleaseKey"
+  is '发布的Key';
+comment on column "InstanceConfig"."ReleaseDeliveryTime"
+  is '配置获取时间';
+comment on column "InstanceConfig"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "InstanceConfig"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index IX_DataChange_LastTime00000 on "InstanceConfig" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -609,6 +788,32 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "Item"
+  is '配置项目';
+-- Add comments to the columns 
+comment on column "Item"."Id"
+  is '主键';
+comment on column "Item"."NamespaceId"
+  is '集群NamespaceId';
+comment on column "Item"."Key"
+  is '配置项Key';
+comment on column "Item"."Value"
+  is '配置项值';
+comment on column "Item"."Comment"
+  is '注释';
+comment on column "Item"."LineNum"
+  is '行号';
+comment on column "Item"."IsDeleted"
+  is 'IsDeleted';
+comment on column "Item"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "Item"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "Item"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "Item"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index DataChange_LastTime00005 on "Item" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -675,6 +880,26 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "Namespace"
+  is '命名空间';
+-- Add comments to the columns 
+comment on column "Namespace"."Id"
+  is '主键';
+comment on column "Namespace"."AppId"
+  is 'AppID';
+comment on column "Namespace"."ClusterName"
+  is 'Cluster Name';
+comment on column "Namespace"."NamespaceName"
+  is 'Namespace Name';
+comment on column "Namespace"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "Namespace"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "Namespace"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "Namespace"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index AppId_ClusterName_NamespaceNa on "Namespace" ("AppId", "ClusterName", "NamespaceName")
   tablespace APOLLOCONFIGDB_DAT
@@ -746,6 +971,22 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "NamespaceLock"
+  is 'namespace的编辑锁';
+-- Add comments to the columns 
+comment on column "NamespaceLock"."Id"
+  is '主键';
+comment on column "NamespaceLock"."NamespaceId"
+  is 'AppID';
+comment on column "NamespaceLock"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "NamespaceLock"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "NamespaceLock"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "NamespaceLock"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index DataChange_LastTime00007 on "NamespaceLock" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -813,6 +1054,34 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "Release"
+  is '发布';
+-- Add comments to the columns 
+comment on column "Release"."Id"
+  is '主键';
+comment on column "Release"."ReleaseKey"
+  is '发布的Key';
+comment on column "Release"."Name"
+  is '发布名字';
+comment on column "Release"."Comment"
+  is '发布说明';
+comment on column "Release"."AppId"
+  is 'AppID';
+comment on column "Release"."ClusterName"
+  is 'ClusterName';
+comment on column "Release"."NamespaceName"
+  is 'namespaceName';
+comment on column "Release"."Configurations"
+  is '发布配置';
+comment on column "Release"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "Release"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "Release"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "Release"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index AppId_ClusterName_GroupName on "Release" ("AppId", "ClusterName", "NamespaceName")
   tablespace APOLLOCONFIGDB_DAT
@@ -880,6 +1149,16 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "ReleaseMessage"
+  is '发布消息';
+-- Add comments to the columns 
+comment on column "ReleaseMessage"."Id"
+  is '自增主键';
+comment on column "ReleaseMessage"."Message"
+  is '发布的消息内容';
+comment on column "ReleaseMessage"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index DataChange_LastTime00009 on "ReleaseMessage" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -947,6 +1226,36 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "ReleaseHistory"
+  is '发布历史';
+-- Add comments to the columns 
+comment on column "ReleaseHistory"."Id"
+  is '主键';
+comment on column "ReleaseHistory"."AppId"
+  is 'AppID';
+comment on column "ReleaseHistory"."ClusterName"
+  is 'ClusterName';
+comment on column "ReleaseHistory"."NamespaceName"
+  is 'namespaceName';
+comment on column "ReleaseHistory"."BranchName"
+  is '发布分支名';
+comment on column "ReleaseHistory"."ReleaseId"
+  is '关联的Release Id';
+comment on column "ReleaseHistory"."PreviousReleaseId"
+  is '前一次发布的ReleaseId';
+comment on column "ReleaseHistory"."Operation"
+  is '发布类型，0: 普通发布，1: 回滚，2: 灰度发布，3: 灰度规则更新，4: 灰度合并回主分支发布，5: 主分支发布灰度自动发布，6: 主分支回滚灰度自动发布，7: 放弃灰度';
+comment on column "ReleaseHistory"."OperationContext"
+  is '发布上下文信息';
+comment on column "ReleaseHistory"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "ReleaseHistory"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "ReleaseHistory"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "ReleaseHistory"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index IX_DataChange_LastTime00001 on  "ReleaseHistory" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -1021,6 +1330,28 @@ tablespace APOLLOCONFIGDB_DAT
     minextents 1
     maxextents unlimited
   );
+-- Add comments to the table 
+comment on table "ServerConfig"
+  is '配置服务自身配置';
+-- Add comments to the columns 
+comment on column "ServerConfig"."Id"
+  is '主键';
+comment on column "ServerConfig"."Key"
+  is '配置项Key';
+comment on column "ServerConfig"."Cluster"
+  is '配置对应的集群，default为不针对特定的集群';
+comment on column "ServerConfig"."Value"
+  is '配置项值';
+comment on column "ServerConfig"."Comment"
+  is '注释';
+comment on column "ServerConfig"."DataChange_CreatedBy"
+  is '创建人邮箱前缀';
+comment on column "ServerConfig"."DataChange_CreatedTime"
+  is '创建时间';
+comment on column "ServerConfig"."DataChange_LastModifiedBy"
+  is '最后修改人邮箱前缀';
+comment on column "ServerConfig"."DataChange_LastTime"
+  is '最后修改时间';
 -- Create/Recreate indexes 
 create index DataChange_LastTime00010 on "ServerConfig" ("DataChange_LastTime")
   tablespace APOLLOCONFIGDB_DAT
@@ -1074,7 +1405,7 @@ insert into "ServerConfig" values (5, 'item.key.length.limit', 'default', '128',
 create sequence ID_SEQ
 minvalue 1
 maxvalue 9223372036854775807
-start with 1
+start with 10
 increment by 1
 cache 20
 cycle
